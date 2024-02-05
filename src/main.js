c73b79c24c5718855eff9adb182ecffb488b729a
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-02-05 20:40:46 +0530

    path change and toggle button for mobile view

--- src/main.js
+++
@@ -1,8 +0,0 @@
-// <!-- <script>
-//               function toggleMenu() {
-//                 document
-//                   .getElementById("navbar-menu")
-//                   .classList.toggle("hidden");
-//                 document.body.classList.toggle("overflow-hidden");
-//               }
-//             </script> -->
